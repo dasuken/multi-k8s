e17f52b8d842144bd41bebb27a8a6c9c23df6171
github daisukenoguchi <daisukenoguemon@gmail.com>
2020-08-09 16:21:04 +0900

    image forwardeing stephen => my repo

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t hoge25/multi-client:latest -t hoge25/multi-client:$SHA ./client
+docker build -t hoge25/multi-server:latest -t hoge25/multi-server:$SHA ./server
+docker build -t hoge25/multi-worker:latest -t hoge25/multi-worker:$SHA ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push hoge25/multi-client:latest
+docker push hoge25/multi-server:latest
+docker push hoge25/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push hoge25/multi-client:$SHA
+docker push hoge25/multi-server:$SHA
+docker push hoge25/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=hoge25/multi-server:$SHA
+kubectl set image deployments/client-deployment client=hoge25/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=hoge25/multi-worker:$SHA
